a4121384a66dd83ebf162f10beb8537df3008084
Lulseged Admasu <lulsegedwork@gmail.comu>
2022-11-27 20:34:58 -0500

    fully responsive

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     <div className='App'>
       <div className='blob blob-1'></div>
       <div className='blob blob-2'></div>
-      <div className='blob blob-3'></div>
+      {/* <div className='blob blob-3'></div> */}
 
       <div className='clock'>
         <img src={clock} alt='' />
